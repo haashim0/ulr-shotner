76abd4ca051ef8e68fbf834e7f2513541c29a1f8
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-10-30 19:54:06 +0530

    Update m.py

--- m.py
+++ m.py
@@ -59,9 +59,7 @@ async def welcome(e):
 
     if stype:
         if stype == 1:
-            domain = "Shorturllink.in"
-        else:
-            domain = "Playdisk.xyz"
+            domain = "metaurls.in"
         await api_logger(chat.id, api_key, stype)
         await client.edit_message(mess,
                                   f"<b>Account Connected Sucessfully ✅\n\nAccount: {username}\nShortner: {domain}</b>")
@@ -116,9 +114,7 @@ async def api(e):
 
     if stype:
         if stype == 1:
-            domain = "Shorturllink.in"
-        else:
-            domain = "Playdisk.xyz"
+            domain = "metaurls.in"
         await api_logger(chat.id, api_key, stype)
         await client.edit_message(mess, f"<b>Account Connected Sucessfully ✅\n\nShortner: {domain}</b>")
     else:
